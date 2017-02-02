879d87ff791c474a4e4c54be8b027b19baf8fb0b
stefanFCB94 <stefan.laeufle@gmail.com>
2017-02-02 23:11:42 +0100

    Added config file support

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,8 @@
 import * as fs from 'fs';
 import * as mime from 'mime';
 import * as path from 'path';
+import * as config from 'config';
+
 
 export interface FileInformation {
     path: string,
@@ -8,11 +10,58 @@ export interface FileInformation {
     type: string
 };
 
-export function getFilesOfDir(dir?: string): Promise<Array<FileInformation>> {
-    dir = (!dir) ? __dirname : path.resolve(dir);
-    
-    return readDir(dir);
+
+// Config-Key for the supported MIME-Types
+const KEY_SUPPORTED_MIME_TYPES: string = 'File_Detection.supported_mime_types';
+
+// Config-Key for the default directories
+const KEY_DEFAULT_DIRECTORIES: string = 'File_Detection.directories';
+
+// Read all supported MIME types from config file
+let supportedMIMETypes: Array<string> = [];
+if ( config.has(KEY_SUPPORTED_MIME_TYPES) ) {
+    supportedMIMETypes = config.get(KEY_SUPPORTED_MIME_TYPES) as Array<string>;
 }
+
+
+export function getFilesOfDir(dir?: string | Array<string>): Promise<Array<FileInformation>> {
+    return new Promise<Array<FileInformation>>((resolve, reject) => {
+        // Check if dir is given
+        // If dir is not given, then take the directories,
+        // which are configured in the config file of the
+        // environment
+        if ( dir == null ) {
+            dir = [];
+            if ( config.has(KEY_DEFAULT_DIRECTORIES) )
+                dir = config.get(KEY_DEFAULT_DIRECTORIES) as Array<string>;
+        }
+
+        // From that point on, just work with a array
+        if ( typeof dir === 'string' )
+            dir = [dir];
+
+        // Read all found Directories
+        const promises: Array<Promise<Array<FileInformation>>> = [];
+        for ( let i: number = 0; i < dir.length; i++ ) {
+            dir[i] = path.resolve(dir[i]);
+            promises.push(readDir(dir[i]));
+        }
+
+        // If all directories are read, give back all found
+        // files in the promise back
+        Promise.all(promises).then((res) => {
+            let files: Array<FileInformation> = [];
+
+            for (let i:number = 0; i < res.length; i++ ) {
+                files = files.concat(res[i]);
+            }
+
+            resolve(files);
+        });        
+    });    
+}
+
+
 
 function readDir(dir: string): Promise<Array<FileInformation>> {
     return new Promise((resolve, reject) => {
@@ -32,6 +81,7 @@ function readDir(dir: string): Promise<Array<FileInformation>> {
                 for (let i:number = 0; i < res.length; i++ ) {
                     files = files.concat(res[i]);
                 }
+                
                 resolve(files);
             });
         });
@@ -42,10 +92,12 @@ function readDir(dir: string): Promise<Array<FileInformation>> {
 
 function getFileInformation(path: string): Promise<Array<FileInformation>> {
     return new Promise((resolve, reject) => {
-        console.log(path);
+        
         fs.stat(path, (err, stats) => {
             if (err) return reject(err);
 
+            // If the path is a directory, read all files from
+            // that directory
             if ( stats.isDirectory() ) {
                 readDir(path)
                     .then((res: Array<FileInformation>) => {
@@ -57,11 +109,22 @@ function getFileInformation(path: string): Promise<Array<FileInformation>> {
                 return;
             }
 
+            // If the path is a file, read the information
+            // of these files
             if ( stats.isFile() ) {
+                const type: string = mime.lookup(path);
+
+                // File will be given back, when no supported
+                // MIME types are configured, or the detected
+                // MIME type is allowed
+                if ( supportedMIMETypes.length > 0 && supportedMIMETypes.indexOf(type) == -1 ) {
+                    return resolve([]);
+                }
+
                 const infos: FileInformation = {
-                    path: path,
+                    path,
                     size: stats.size,
-                    type: mime.lookup(path)
+                    type
                 };
 
                 return resolve([infos]);
@@ -71,6 +134,6 @@ function getFileInformation(path: string): Promise<Array<FileInformation>> {
 }
 
 
-getFilesOfDir('./dist').then((res) => {
+getFilesOfDir().then((res) => {
     console.log(res); 
 });
